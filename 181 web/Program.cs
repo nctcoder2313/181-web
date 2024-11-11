739ac54417f79f480e87675bc3482e8bcc42e737
rairaisarip <sarip.rh783@s.msumain.edu.ph>
2024-11-11 05:33:42 -0800

    Now, we have several messages

--- 181 web/Program.cs
+++ 181 web/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Raisa!How was your first try? Let's try again");
+Console.WriteLine("Hello, Raisa!How was your first try? Let's try again"); Console.WriteLine("Hello, Raisa!How was your first try? Let's try again"); Console.WriteLine("Hello, Raisa!How was your first try? Let's try again"); Console.WriteLine("Hello, Raisa!How was your first try? Let's try again"); Console.WriteLine("Hello, Raisa!How was your first try? Let's try again"); Console.WriteLine("Hello, Raisa!How was your first try? Let's try again");
